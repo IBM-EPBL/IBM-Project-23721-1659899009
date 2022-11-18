ea6ef27b06c71c7482f2d614294348e28b8cbd71
pramod5425 <113779801+pramod5425@users.noreply.github.com>
2022-11-19 00:51:28 +0530

    Add files via upload

---
+++ APPLICATION BUILDING/RUN THE APP/RUN THE APP.py
@@ -0,0 +1,218 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
+from skimage import feature
+from imutils import build_montages
+from imutils import paths
+import numpy as np
+import cv2
+import os
+import pickle
+import PIL
+#import tensorflow as tf
+
+#from tensorflow import keras
+#from tensorflow.keras import layers
+#from tensorflow.keras.models import Sequential
+trainingpath=r"C:\Users\NITIYASRI\Downloads\wave\wave\training"
+testingpath=r"C:\Users\NITIYASRI\Downloads\wave\wave\testing"
+def quantify_image(image):
+    features = feature.hog(image, orientations=9,
+                           pixels_per_cell=(10, 10),
+                           cells_per_block=(2, 2),
+                           transform_sqrt=True,
+                           block_norm="L1")
+    return features
+
+
+def load_split(path):
+    imagePaths = list(paths.list_images(path))
+    data = []
+    labels = []
+
+    for imagePath in imagePaths:
+        label = imagePath.split(os.path.sep)[-2]
+
+        image = cv2.imread(imagePath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (200, 200))
+
+        image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+        features = quantify_image(image)
+
+        data.append(features)
+        labels.append(label)
+
+    return (np.array(data), np.array(labels))
+print("[INFO] loading data...")
+(X_train, y_train) = load_split(trainingpath)
+(X_test, y_test) = load_split(testingpath)
+le = LabelEncoder()
+y_train = le.fit_transform(y_train)
+y_test = le.transform(y_test)
+print(X_train.shape,y_train.shape)
+print("[INFO] training model")
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+testingpath=list(paths.list_images(testingpath))
+idxs=np.arange(0,len(testingpath))
+idxs=np.random.choice(idxs,size=(25,),replace=False)
+images=[]
+for i in idxs:
+    image = cv2.imread(testingpath[i])
+    output = image.copy()
+
+    # load the input image,convert to grayscale and resize
+
+    output = cv2.resize(output, (128, 128))
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.resize(image, (200, 200))
+    image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+    # quantify the image and make predictions based on the  extracted feature using last trained random forest
+    features = quantify_image(image)
+    preds = model.predict([features])
+    label = le.inverse_transform(preds)[0]
+    # the set of output images
+    if label == "healthy":
+        color = (0, 255, 0)
+    else:
+        color = (0, 0, 255)
+
+    cv2.putText(output, label, (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    images.append(output)
+
+# creating a montage
+montage = build_montages(images, (128, 128), (5, 5))[0]
+cv2.imshow("Output", montage)
+predictions = model.predict(X_test)
+
+cm = confusion_matrix(y_test, predictions).flatten()
+print(cm)
+(tn, fp, fn, tp) = cm
+accuracy = (tp + tn) / float(cm.sum())
+print(accuracy)
+pickle.dump(model,open('parkinson.pkl','wb'))
+import pickle
+import cv2
+from skimage import feature
+from flask import Flask, request, render_template
+import os.path
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    return render_template('predict.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        filepath = os.path.join(basepath, "uploads", secure_filename(f.filename))
+        f.save(filepath)
+        print(" (INFO] loading model...")
+        model = pickle.loads(open('parkinson.pkl', "rb").read())
+        image = cv2.imread(filepath)
+
+        output = image.copy
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (200, 200))
+        image = cv2.threshold(image, 0, 255,
+                              cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+        features = feature.hog(image, orientations=9,
+                               pixels_per_cell=(10, 10), cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1")
+        preds = model.predict([features])
+        print(preds)
+        ls = ["healthy", "parkinson"]
+        result = ls[preds[0]]
+        color = (0, 255, 0) if result == "healthy" else (0, 0, 255)
+
+        return result
+    return None
+    return 'file uploaded successfully'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
